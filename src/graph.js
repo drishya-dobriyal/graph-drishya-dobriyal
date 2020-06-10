2645dfe2bba771daae484e54bc215ba572a28996
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-06-10 19:35:05 +0530

    created bfs funtion

--- src/graph.js
+++ src/graph.js
@@ -1,11 +1,31 @@
-//Example 
-// Pairs => [[from,to],[to,from]]
-// Source => from
-// To => to 
-// Should return true.
-
-const bfs = function(pairs,source,target){
-  
+const createList = details => {
+  const list = {};
+  details.forEach(data => {
+    if (list[data[0]]) list[data[0]].push(data[1]);
+    else list[data[0]] = [data[1]];
+  });
+  return list;
 };
 
-module.exports = {bfs};
+const bfs = function(pairs, source, target) {
+  const list = createList(pairs);
+  const queue = [];
+  const visited = [];
+  queue.push(source);
+
+  while (queue.length != 0) {
+    const curr = queue.shift();
+    if (curr === target) return true;
+    visited.push(curr);
+    for (let i = 0; i < list[curr].length; i++) {
+      const temp = list[curr][i];
+      if (!visited.includes(temp) && !queue.includes(temp)) {
+        queue.push(temp);
+      }
+    }
+  }
+
+  return false;
+};
+
+module.exports = { bfs };
